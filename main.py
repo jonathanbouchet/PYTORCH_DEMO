6534cae98efda8363d41eaf8ee4927f0a4b47b00
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2025-01-23 14:33:01 -0800

    refactoring of st.session_state initialization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+# from templates.settings import initialize_session_state
 
 st.set_page_config(layout="wide",
                    page_title="pytorch demo",
@@ -12,5 +13,6 @@ settings = st.Page("templates/settings.py", title="prepare data")
 display = st.Page("templates/display.py", title="display data")
 
 if __name__ == "__main__":
+    # initialize_session_state()
     pg = st.navigation([settings, display])
     pg.run()
